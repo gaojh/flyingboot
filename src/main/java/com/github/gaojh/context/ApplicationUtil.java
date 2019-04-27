c6ec97568d0777754d1da20eb7b3c24362d21b68
高建华 <gaojh_84@hotmail.com>
2019-04-27 16:51:43 +0800

    修改包路径，以便上maven中央库

--- src/main/java/com/github/gaojh/context/ApplicationUtil.java
+++ src/main/java/com/github/gaojh/context/ApplicationUtil.java
@@ -1,7 +1,5 @@
 package com.github.gaojh.context;
 
-import com.github.gaojh.config.ApplicationConfig;
-import com.github.gaojh.config.Environment;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,12 +12,4 @@ public class ApplicationUtil {
     @Setter
     @Getter
     public static ApplicationContext applicationContext;
-
-    @Getter
-    @Setter
-    public static ApplicationConfig applicationConfig;
-
-    @Setter
-    @Getter
-    public static Environment environment;
 }
